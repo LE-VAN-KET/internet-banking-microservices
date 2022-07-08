c63622f0b9ef9b4c3605389d5d9e0a80ea00a7f8
anhle1512001@gmail.com <anhle1512001@gmail.com>
2022-07-08 09:15:34 +0700

    service registry

---
+++ internet-banking-service-register/src/main/java/com/xomtrodut/internetbankingserviceregister/InternetBankingServiceRegisterApplication.java
@@ -0,0 +1,15 @@
+package com.xomtrodut.internetbankingserviceregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class InternetBankingServiceRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternetBankingServiceRegisterApplication.class, args);
+    }
+
+}
---
+++ internet-banking-service-register/src/test/java/com/xomtrodut/internetbankingserviceregister/InternetBankingServiceRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xomtrodut.internetbankingserviceregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetBankingServiceRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
